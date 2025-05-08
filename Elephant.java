ab33dfe0d0a24e9870cd0556cb02ccb7156e7071
samcam72 <64269315+samcam72@users.noreply.github.com>
2025-05-08 13:48:51 -0400

    Create Elephant.java

---
+++ Elephant.java
@@ -0,0 +1,10 @@
+public class Elephant extends Animal {
+
+    public Elephant(String name) {
+        super(name, "Elephant");
+    }
+
+    public void makeSound() {
+        System.out.println(name + " the Elephant says: Trumpet!");
+    }
+}
